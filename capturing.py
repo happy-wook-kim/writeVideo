ba8d25c9642f72473e4143d747df6780bdc85bb4
catdog6210 <catdog6210@gmail.com>
2022-06-10 11:01:21 +0900

    [UPDATE] capturing _ 카메라 주소 변경 + 15/30 프레임 영상 추가

--- capturing.py
+++ capturing.py
@@ -4,34 +4,40 @@ import datetime
 
 
 def get_capture(file_name:str):
-    url = 'rtmp://52.78.104.124:1935/live/cam1'
+    url = 'rtmp://52.78.104.124:1935/live/cam2'
     url2 = 'http://52.78.104.124:1935/vod/mp4:sample.mp4/playlist.m3u8'
 
-    # cap = cv2.VideoCapture(url)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(url)
+    # cap = cv2.VideoCapture(0)
 
     name = './video/cam1' + '_' + file_name + '.mp4'
+    name_15f = './video/cam1' + '_' + file_name + '_15f' + '.mp4'
+    name_30f = './video/cam1' + '_' + file_name + '_30f' + '.mp4'
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(name, fourcc, 10.0, (int(cap.get(3)), (int(cap.get(4)))))
+    # out_15f = cv2.VideoWriter(name_15f, fourcc, 15.0, (int(cap.get(3)), (int(cap.get(4)))))
+    out_30f = cv2.VideoWriter(name_30f, fourcc, 30.0, (int(cap.get(3)), (int(cap.get(4)))))
 
     start = datetime.datetime.now()
     count = 1
+    print('capture start time: ', start)
     while True:
         try:
             ret, frame = cap.read()
             h, w, _ = frame.shape
-            # print('size of frame', w, h)
             count += 1
             if ret is True:
                 # frame = np.clip(contrast * frame + brightness, 0, 255)
                 cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX)
                 cv2.imshow('cam1', frame)
+                end = datetime.datetime.now()
+                elapsed = end - start
 
                 if count % 3 == 0:
                     out.write(frame)
-                end = datetime.datetime.now()
-                elapsed = end - start
-                print('time elapsed', elapsed.seconds)
+                    # out_15f.write(frame)
+                    out_30f.write(frame)
+                    print('time elapsed', elapsed.seconds)
 
                 if elapsed.seconds % 10 == 0 and elapsed.seconds > 0:
                     # i += 1
