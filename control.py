a8eccc95a930755e2809e3bcbd7f990b5f4f3fa7
catdog6210 <catdog6210@gmail.com>
2022-06-10 11:01:43 +0900

    [UPDATE] control _ 30프레임 영상 추가 업로드

--- control.py
+++ control.py
@@ -27,15 +27,15 @@ while True:
         # file_name = str(now.now().today()).rsplit('.', 1)[0]
         # 파일명을 원하는 형태로 만든다.
         file_name = year + "_" + month + "_" + day + "_" + hour + ":" + minute + ":" + second
-        
         try:
             # 스트리밍 영상 녹화 쓰레드
             t = threading.Thread(get_capture(file_name))
             t.start()
+            print('capture thread start')
         except:
             # 통신에러 등 에러가 발생했을 때 10초 대기 후 while문을 다시 돈다.
-            print('스트리밍 영상 캡처에 에러가 발생하였습니다. 10초 후 다시 동작을 시도합니다.')
-            sleep(10)
+            print('스트리밍 영상 캡처에 에러가 발생하였습니다. 2초 후 다시 동작을 시도합니다.')
+            sleep(2)
         else:
             # 스트리밍 영상 녹화 후 서버로 전송
             # 대기 시간 초기화
@@ -43,15 +43,21 @@ while True:
             elapsed = end - start
 
             name = './video/cam1' + '_' + file_name + '.mp4'
+            # name_15f = './video/cam1' + '_' + file_name + '_15f' + '.mp4'
+            name_30f = './video/cam1' + '_' + file_name + '_30f' + '.mp4'
             
             print('전송할 영상: ', name, "\n" , 'opencv 읽는 영상: ', name)
-            video = cv2.VideoCapture(name)
+            # video = cv2.VideoCapture(name)
             
             # 서버로 영상 전송
             upload = Uploads(name, '')
             try:
                 upload_thread = threading.Thread(upload.upload_video(name))
                 upload_thread.start()
+                # upload_thread_15f = threading.Thread(upload.upload_video(name_15f))
+                # upload_thread_15f.start()
+                upload_thread_30f = threading.Thread(upload.upload_video(name_30f))
+                upload_thread_30f.start()
             except (FileNotFoundError):
                 print('동영상 파일을 찾을 수 없습니다.')
             except ConnectionError:
