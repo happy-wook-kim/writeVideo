56db3d976538c5637fc0692b7d0b2d8294989330
catdog6210 <catdog6210@gmail.com>
2022-06-14 14:13:36 +0900

    [UPDATE] capturing _ SBC에서 캡쳐링 하기 위해 수정

--- capturing.py
+++ capturing.py
@@ -4,19 +4,29 @@ import datetime
 
 
 def get_capture(file_name:str):
-    url = 'rtmp://52.78.104.124:1935/live/cam1'
-    url2 = 'http://52.78.104.124:1935/vod/mp4:sample.mp4/playlist.m3u8'
+    # url = 'rtmp://52.78.104.124:1935/live/cam1'
+    # url2 = 'http://52.78.104.124:1935/vod/mp4:sample.mp4/playlist.m3u8'
 
-    cap = cv2.VideoCapture(url)
+    # jetson nano에 부착된 usb 위치에 따른 카메라 위치
+    cam_location = 2;
+    
+    # adddai가 운영하는 카메라 인덱스
+    cam_numbering = 4;
+
+    # cap = cv2.VideoCapture(url)
+    cap = cv2.VideoCapture(cam_location)
     # cap = cv2.VideoCapture(0)
 
-    name = './video/cam1' + '_' + file_name + '.mp4'
-    name_15f = './video/cam1' + '_' + file_name + '_15f' + '.mp4'
-    name_30f = './video/cam1' + '_' + file_name + '_30f' + '.mp4'
+    FOLDER = './video/cam'
+    WRITE_LOCATION = FOLDER + str(cam_numbering)
+    
+    name = WRITE_LOCATION + '_' + file_name + '.mp4'
+    # name_15f = WRITE_LOCATION + '_' + file_name + '_15f' + '.mp4'
+    # name_30f = WRITE_LOCATION + '_' + file_name + '_30f' + '.mp4'
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(name, fourcc, 10.0, (int(cap.get(3)), (int(cap.get(4)))))
     # out_15f = cv2.VideoWriter(name_15f, fourcc, 15.0, (int(cap.get(3)), (int(cap.get(4)))))
-    out_30f = cv2.VideoWriter(name_30f, fourcc, 30.0, (int(cap.get(3)), (int(cap.get(4)))))
+    # out_30f = cv2.VideoWriter(name_30f, fourcc, 30.0, (int(cap.get(3)), (int(cap.get(4)))))
 
     start = datetime.datetime.now()
     count = 1
@@ -36,7 +46,7 @@ def get_capture(file_name:str):
                 if count % 3 == 0:
                     out.write(frame)
                     # out_15f.write(frame)
-                    out_30f.write(frame)
+                    # out_30f.write(frame)
                 
                 if count % 6 == 0:
                     print('time elapsed', elapsed.seconds)
@@ -51,6 +61,8 @@ def get_capture(file_name:str):
                 # 영상을 15분 기준으로 끊어서 저장한다.
                 if elapsed.seconds > 900:
                     break
+            else:
+                print('열 수  없음')
 
         except:
             break
